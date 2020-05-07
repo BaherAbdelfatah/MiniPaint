3e5fd3ee24a59cb2204d90e2bcd21a153ef5d8b1
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-05-07 05:48:00 +0200

    Override onDraw()

--- app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
@@ -21,5 +21,9 @@ class MyCanvasView(context: Context) : View(context) {
         extraCanvas.drawColor(backgroundColor)
     }
 
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
 
 }
