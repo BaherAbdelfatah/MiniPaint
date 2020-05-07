907748f8356a01a0265680dfc6f3d71fadcfa0ae
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-05-07 06:10:03 +0200

    Add Frame

--- app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
@@ -1,10 +1,7 @@
 package com.baherandroid.minipaint
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -44,17 +41,26 @@ class MyCanvasView(context: Context) : View(context) {
 
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
+    private lateinit var frame: Rect
+
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        // Calculate a rectangular frame around the picture.
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+
+        // Draw a frame around the canvas.
+        canvas.drawRect(frame, paint)
     }
 
     private fun touchStart() {
