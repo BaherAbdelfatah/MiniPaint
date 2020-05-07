2286abde0596cc31ef9ccef9a0abda32da1f62e7
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-05-07 05:45:45 +0200

    Override onSizeChanged()

--- app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/baherandroid/minipaint/MyCanvasView.kt
@@ -1,7 +1,25 @@
 package com.baherandroid.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context) : View(context) {
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+
 }
